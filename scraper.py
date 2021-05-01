3893f949666a0c45389a7741d2042275aaa2a042
Luis yChun <yongchun903@gmail.com>
2021-05-01 17:24:16 +0800

    feat: convert captured result into yyyy-mm-dd format

--- scraper.py
+++ scraper.py
@@ -52,50 +52,59 @@ from datetime import datetime, timedelta
 #     except Exception as e:
 #         raise e
 
+def format_date(holidays_list):
+  new_list = []
+  for idx in range(len(holidays_list)):
+    new_list.append(datetime.strptime(holidays_list[idx], "%d %B %Y").strftime('%Y-%m-%d'))
+  return new_list
+
+
 # program start here
 def main():
 
-    # driver = webdriver.Chrome(ChromeDriverManager().install())
-    options = webdriver.ChromeOptions()
-    options.add_argument('headless')
+  # driver = webdriver.Chrome(ChromeDriverManager().install())
+  options = webdriver.ChromeOptions()
+  options.add_argument('headless')
 
-    driver = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=options)
+  driver = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=options)
 
-    URL = 'https://www.mom.gov.sg/employment-practices/public-holidays'
+  URL = 'https://www.mom.gov.sg/employment-practices/public-holidays'
 
-    driver.get(URL)
+  driver.get(URL)
 
-    # execute script to scroll down the page
-    driver.execute_script(
-        "window.scrollTo(0, document.body.scrollHeight);var lenOfPage=document.body.scrollHeight;return lenOfPage;")
-    # sleep for 30s
-    time.sleep(10)
+  # execute script to scroll down the page
+  driver.execute_script(
+      "window.scrollTo(0, document.body.scrollHeight);var lenOfPage=document.body.scrollHeight;return lenOfPage;")
+  # sleep for 30s
+  time.sleep(10)
 
+  # holidays list
+  holidays_list = []
 
-    # holidays list
-    holidays_list = []
+  # holidays row XPath
+  # 2020 //*[@id="pagecontent_0_documentcontent_0_RptHolidayTabContent_RptHoliday_0_Row_0"]/td[1]/text()
+  # 2021 //*[@id="pagecontent_0_documentcontent_0_RptHolidayTabContent_RptHoliday_1_Row_0"]/td[1]/text()
+  # 2022 //*[@id="pagecontent_0_documentcontent_0_RptHolidayTabContent_RptHoliday_2_Row_0"]/td[1]/text()
 
-    # holidays row XPath
-    # 2020 //*[@id="pagecontent_0_documentcontent_0_RptHolidayTabContent_RptHoliday_0_Row_0"]/td[1]/text()
-    # 2021 //*[@id="pagecontent_0_documentcontent_0_RptHolidayTabContent_RptHoliday_1_Row_0"]/td[1]/text()
-    # 2022 //*[@id="pagecontent_0_documentcontent_0_RptHolidayTabContent_RptHoliday_2_Row_0"]/td[1]/text()
+  content_wrapper = driver.find_elements_by_xpath("//tr[contains(@id, '_RptHoliday_1_')]")
+  for content in content_wrapper:
+      actual_date = content.find_element_by_class_name('footable-first-column')
+      if("\n" in actual_date.text):
+        split_date = actual_date.text.split("\n")
+        for split in split_date:
+          holidays_list.append(split)
+      else:
+        holidays_list.append(actual_date.text)
 
-    content_wrapper = driver.find_elements_by_xpath("//tr[contains(@id, '_RptHoliday_1_')]")
-    for content in content_wrapper:
-        actual_date = content.find_element_by_class_name('footable-first-column')
-        if("\n" in actual_date.text):
-          split_date = actual_date.text.split("\n")
-          for split in split_date:
-            holidays_list.append(split)
-        else:
-          holidays_list.append(actual_date.text)
+  # format date
+  formatted_list = format_date(holidays_list)
 
-    if not content_wrapper:
-        print("Requested URL is invalid. Please check.")
-    
-    driver.quit()
+  if not content_wrapper:
+      print("Requested URL is invalid. Please check.")
+  
+  driver.quit()
 
-    print(holidays_list)
+  print(formatted_list)
 
 
 main()
